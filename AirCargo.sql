fa7060905fb64acdc24a4c1f1e6f48213f8d298e
Jimmy Patel <47034967+jimit-patel@users.noreply.github.com>
2023-04-17 23:56:39 +0530

    Added 2SQL files (Aircargo & Employee)

---
+++ AirCargo.sql
@@ -0,0 +1,208 @@
+-- 1. Create a database named AirCargo
+CREATE DATABASE IF NOT EXISTS AirCargo;
+USE AirCargo;
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM passengers_on_flights;
+
+SELECT *
+FROM routes
+ORDER BY route_id;
+
+SELECT *
+FROM ticket_details
+ORDER BY aircraft_id;
+
+
+-- 2. Query to modify table and asign Primary key to relavent column
+ALTER TABLE customer
+MODIFY customer_id VARCHAR(10) PRIMARY KEY;
+
+ALTER TABLE routes
+MODIFY route_id VARCHAR(10) PRIMARY KEY;
+
+
+-- 3. Query to display the full name of the customer by extracting the first name and last name from the customer table.
+SELECT CONCAT(first_name, " ", last_name) "Full Name"
+FROM customer;
+
+
+-- 4. Query to identify the number of passengers and total revenue in business class flight.
+SELECT COUNT(customer_id) AS "Total Passengers", SUM(price_per_ticket * no_of_tickets) AS "Total Revenue"
+FROM ticket_details
+WHERE class_id = "bussiness";
+
+
+-- 5. Query to display all the passengers (customers) who have travelled in routes 01 to 25.
+SELECT DISTINCT(CONCAT(first_name, " ", last_name)) "Full Name", date_of_birth "Birth Date", gender "Gender"
+FROM customer
+WHERE customer_id 
+	IN (
+		SELECT customer_id
+		FROM passengers_on_flights
+		WHERE route_id BETWEEN 1 AND 25
+	);
+    
+    
+-- 6. Query to extract the customers who have registered and booked a ticket.
+SELECT DISTINCT(CONCAT(first_name, " ", last_name)) "Full Name", date_of_birth "Birth Date", gender "Gender"
+FROM customer
+WHERE customer_id 
+	IN (
+		SELECT DISTINCT(customer_id)
+		FROM ticket_details
+	);
+    
+    
+-- 7. Query to identify the customer’s first name and last name based on their customer ID and brand (Emirates).
+SELECT first_name "First Name", last_name "Last Name"
+FROM customer
+WHERE customer_id 
+	IN (
+		SELECT DISTINCT(customer_id)
+		FROM ticket_details
+        WHERE brand = "Emirates"
+	);
+    
+    
+-- 8. Query to identify the customers who have travelled by Economy Plus class.
+SELECT DISTINCT(CONCAT(first_name, " ", last_name)) "Full Name", date_of_birth "Birth Date", gender "Gender"
+FROM customer
+WHERE customer_id 
+	IN (
+		SELECT DISTINCT(customer_id)
+		FROM passengers_on_flights
+        WHERE class_id LIKE "%Plus"
+	);
+
+
+-- 9. Query to identify whether the revenue has crossed 10000.
+SELECT IF(SUM(price_per_ticket * no_of_tickets) > 10000, "Yes", "No") AS "Revenue >10k"
+FROM ticket_details;
+
+
+-- 11 Query to find the maximum ticket price for each class using window functions on the ticket_details table.
+SELECT class_id as "class", MAX(price_per_ticket)
+FROM ticket_details
+GROUP BY class_id;
+
+
+-- 12. Query to extract the passengers whose route ID is 4 by improving the speed and performance of the passengers_on_flights table.
+CREATE INDEX idx_route_id ON passengers_on_flights (route_id);
+SELECT DISTINCT(CONCAT(first_name, " ", last_name)) "Full Name", date_of_birth "Birth Date", gender "Gender"
+FROM customer
+WHERE customer_id 
+	IN (
+		SELECT DISTINCT(customer_id)
+		FROM passengers_on_flights
+        WHERE route_id = 4
+	);
+
+
+-- 13. For the route ID 4, write a query to view the execution plan of the passengers_on_flights table.
+
+-- 14. Query to calculate the total price of all tickets booked by a customer across different aircraft IDs.
+SELECT aircraft_id " Aircraft ID", SUM(price_per_ticket) "Total Price"
+FROM ticket_details
+GROUP BY aircraft_id;
+
+
+-- 15. Query to create a view with only business class customers along with the brand of airlines.
+CREATE OR REPLACE VIEW business_class_customer AS
+	SELECT customer_id, brand
+    FROM ticket_details
+    WHERE class_id = "Busscustomer_idiness";
+
+    
+-- 16. Query to create a stored procedure to get the details of all passengers flying between a range of routes defined in run time.
+-- START PROCEDURE
+DROP PROCEDURE IF EXISTS passengers;
+
+DELIMITER $$
+
+USE aircargo$$
+CREATE PROCEDURE passengers(start_range INT, end_range INT)
+BEGIN
+	SELECT DISTINCT(CONCAT(first_name, " ", last_name)) "Full Name", date_of_birth "Birth Date", gender "Gender"
+	FROM customer
+	WHERE customer_id 
+		IN (
+			SELECT DISTINCT(customer_id)
+			FROM passengers_on_flights
+			WHERE route_id BETWEEN start_range AND end_range
+		);
+END$$
+
+DELIMITER ;
+;
+
+CALL passengers(20,30);
+-- END PROCEDURE
+
+
+-- 17. Query to create a stored procedure that extracts all the details from the routes table where the travelled distance is more than 2000 miles.
+-- START PROCEDURE
+DROP PROCEDURE IF EXISTS routes_2000;
+
+DELIMITER $$
+
+USE aircargo$$
+CREATE PROCEDURE routes_2000()
+BEGIN
+	SELECT *
+	FROM routes
+	WHERE distance_miles > 2000;
+END$$
+
+DELIMITER ;
+;
+
+CALL routes_2000();
+-- END PROCEDURE
+
+
+-- 18. Query to create a stored procedure that groups the distance travelled by each flight into three categories. 
+-- The categories are, short distance travel (SDT) for >=0 AND <= 2000 miles, intermediate distance travel (IDT) for >2000 AND <=6500, and long-distance travel (LDT) for >6500.
+
+-- START FUNCTION
+DROP FUNCTION IF EXISTS distance_category;
+
+DELIMITER $$
+
+CREATE FUNCTION distance_category(dist INT) RETURNS VARCHAR(40)
+DETERMINISTIC
+BEGIN
+    
+	CASE 
+		WHEN dist >= 0 AND dist <= 2000 THEN  RETURN("Short Distance Travel");
+		WHEN dist > 2000 AND dist <= 6500 THEN  RETURN("Intermediate Distance Travel"); 
+		WHEN dist >= 6500 THEN  RETURN("Long Distance Travel"); 
+	ELSE 
+		RETURN("");
+	END CASE;
+
+END$$
+
+DELIMITER ;
+-- END FUNCTION 
+
+-- START PROCEDURE
+DROP PROCEDURE IF EXISTS distance;
+
+DELIMITER $$
+
+USE aircargo$$
+CREATE PROCEDURE distance()
+BEGIN
+	SELECT route_id "Route ID", flight_num "Flight Number", aircraft_id "Aircraft ID", distance_miles "Distance(Miles)", distance_category(distance_miles) as "Distance Category"
+	FROM routes;
+END$$
+
+DELIMITER ;
+;
+
+CALL distance();
+-- END PROCEDURE
---
+++ ScieanceQtech_Employee.sql
@@ -0,0 +1,162 @@
+-- 1. Create a database named ScienceQtech_employee
+CREATE DATABASE IF NOT EXISTS ScienceQtech_employee;
+USE ScienceQtech_employee;
+
+SELECT *
+FROM data_science_team;
+
+SELECT *
+FROM emp_record_table;
+
+SELECT *
+FROM proj_table;
+
+
+-- 2. Asigned Primary Key to emp_id and project_id column of data_science_team & proj_table respectively.
+ALTER TABLE data_science_team
+MODIFY emp_id VARCHAR(20) PRIMARY KEY;
+
+ALTER TABLE proj_table
+MODIFY project_id VARCHAR(20) PRIMARY KEY;
+
+
+-- 3. Query to fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the employee record table.
+SELECT emp_id, first_name, last_name, gender, dept 
+FROM emp_record_table;
+
+
+-- 4. Query to fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPARTMENT, and EMP_RATING if the EMP_RATING is less than 2, more than 4 and between 2-4.
+SELECT first_name, last_name, gender, dept, emp_rating FROM emp_record_table
+WHERE emp_rating < 2;
+
+SELECT first_name, last_name, gender, dept, emp_rating FROM emp_record_table
+WHERE emp_rating > 4;
+
+SELECT first_name, last_name, gender, dept, emp_rating FROM emp_record_table
+WHERE emp_rating BETWEEN 2 AND 4;
+
+
+-- 5. Query to concatenate the FIRST_NAME and the LAST_NAME of employees of the Finance department.
+SELECT CONCAT(first_name, " ", last_name) as "NAME" FROM emp_record_table
+WHERE dept = "finance";
+
+
+-- 6. Query to fetch list only of those employees who have someone reporting to them. Also, show the number of reporters (including the President).
+SELECT e.emp_id, e.first_name, e.last_name, e2.reporting_no 
+FROM emp_record_table e
+INNER JOIN (
+	SELECT manager_id, COUNT(manager_id) as reporting_no 
+    FROM emp_record_table 
+    GROUP BY manager_id
+    ) e2
+ON e.emp_id = e2.manager_id;
+
+
+-- 7. Query to fetch list all the employees from the healthcare and finance departments using union.
+SELECT * FROM emp_record_table 
+WHERE dept = 'mealthcare'
+UNION
+SELECT * FROM emp_record_table 
+WHERE dept = 'finance';
+
+
+-- 8. Query to list down employee details such as EMP_ID, FIRST_NAME, LAST_NAME, ROLE, DEPARTMENT, EMP_RATING grouped by dept, 
+-- and the respective employee rating along with the max emp rating for the department.
+SELECT emp_id, first_name, last_name, role, dept, emp_rating, MAX(emp_rating) OVER (PARTITION BY dept) as 'MAX_Rating' 
+FROM emp_record_table;
+
+
+-- 9. Query to calculate the minimum and the maximum salary of the employees for each role.
+SELECT emp_id, first_name, last_name, role, salary, MAX(salary) OVER (PARTITION BY role) as 'MAX_Salary', MIN(salary) OVER (PARTITION BY role) as 'MIN_Salary' 
+FROM emp_record_table;
+
+
+-- 10. Query to assign ranks to each employee based on their experience.
+SELECT emp_id, first_name, last_name, exp,  RANK() OVER (ORDER BY exp) as emp_rank
+FROM emp_record_table
+ORDER BY emp_rank DESC;
+
+SELECT emp_id, first_name, last_name, exp,  DENSE_RANK() OVER (ORDER BY exp) as emp_rank 
+FROM emp_record_table
+ORDER BY emp_rank DESC;
+
+-- 11. Query to create a view that displays employees in various countries whose salary is more than six thousand.
+CREATE OR REPLACE VIEW emp_country AS
+    SELECT emp_id, first_name, last_name, country 
+    FROM emp_record_table
+    WHERE salary > 6000;
+
+
+-- 12. Query to find employees with experience of more than ten years.
+SELECT emp_id, first_name, last_name, exp 
+FROM emp_record_table
+WHERE emp_id IN (
+			SELECT emp_id 
+			FROM emp_record_table 
+			WHERE exp > 10);
+
+
+-- 13. Query to create a stored procedure to retrieve the details of the employees whose experience is more than three years.
+-- START PROCEDURE
+DROP PROCEDURE IF EXISTS exp_3;
+
+DELIMITER $$
+
+USE ScienceQtech_employee$$
+CREATE PROCEDURE exp_3()
+BEGIN
+	SELECT emp_id, first_name, last_name, exp 
+    FROM emp_record_table 
+    WHERE exp > 3;
+END$$
+
+DELIMITER ;
+;
+
+CALL exp_3();
+-- END PROCEDURE
+
+
+-- 14. Query to create stored functions in the project table to check whether the job profile assigned to each employee in the data science team matches the organization’s set standard.
+-- START FUNCTION
+DROP FUNCTION IF EXISTS Project_assignment;
+
+DELIMITER $$
+
+CREATE FUNCTION Project_assignment(id VARCHAR(20)) RETURNS VARCHAR(40)
+DETERMINISTIC
+BEGIN
+	DECLARE standard VARCHAR(40);
+	DECLARE experiance INT;
+    
+    SELECT exp INTO experiance 
+    FROM emp_record_table e
+    WHERE e.emp_id = id;
+    
+	CASE 
+		WHEN experiance <= 2 THEN  SET standard = "JUNIOR DATA SCIENTIST";
+		WHEN experiance BETWEEN 2 AND 5 THEN  SET standard = "ASSOCIATE DATA SCIENTIST" ; 
+		WHEN experiance BETWEEN 5 AND 10 THEN  SET standard = "SENIOR DATA SCIENTIST"; 
+		WHEN experiance BETWEEN 10 AND 12 THEN  SET standard = "LEAD DATA SCIENTIST";
+		WHEN experiance > 12 THEN  SET standard = "MANAGER"; 
+	ELSE 
+		SET standard = "";
+	END CASE;
+    RETURN(standard);
+END$$
+
+DELIMITER ;
+
+SELECT emp_id, project_assignment(emp_id) AS standard, EXP 
+FROM emp_record_table;
+-- END FUNCTION 
+
+
+-- 15.Query to calculate the bonus for all the employees, based on their ratings and salaries (Bonus formula: 5% of salary * employee rating).
+SELECT emp_id, first_name,emp_rating, salary, ROUND(0.05*emp_rating*salary) AS bonus
+FROM emp_record_table;   
+
+
+-- 16. Query to calculate the average salary distribution based on the continent and country. Take data from the employee record table.
+SELECT continent, country, ROUND(AVG(salary) OVER (PARTITION BY continent,country)) AS avg_salary 
+FROM emp_record_table;
